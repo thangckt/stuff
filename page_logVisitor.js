de402e6bc9d4c534d96a26e39c6da7ed76e60d0b
C. Thang Nguyen <thangckt@gmail.com>
2026-03-06 02:39:15 +0900

    Update page_logVisitor.js

--- page_logVisitor.js
+++ page_logVisitor.js
@@ -238,10 +238,11 @@
         }
     }
 
-    // Filter visitors based on IP, ASN, or other criteria (optional)
+    // Filter visitors based on IP, ASN, or other fields
     const blackList1 = [ // Define blacklist list-of-dictionaries
-        { ip: '114.70.12.225', asn: 'AS45379', browser: 'Chrome-145', os: 'Linux-Unk' }, // item 1
-        { browser: 'Chrome-145', os: 'Windows' }, // item 2
+        { ip: '114.70.12.225', browser: 'Chrome-145', os: 'Linux-Unk' }, // CANLab desktop
+        { ip: '180.228.220.66', browser: 'Chrome-145', os: 'Linux-Unk' }, // Home laptop
+        { ip: '180.228.220.66', browser: 'Chrome-145', os: 'Android-10' }, // Home mobile
     ];
 
     function checkIfBlocked(visitorInfo, blackList) {
